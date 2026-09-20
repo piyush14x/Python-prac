658782ba44e97ba711a9826b8fca722acf201834
PIYUSH SAINI <piyushsaini@piyushs-MacBook-Air.local>
2026-09-20 20:45:24 +0530

    guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,22 @@
+import random
+
+number = random.randint(1, 100)
+attempts = 0
+
+print("🎯 Number Guessing Game")
+print("I have selected a number between 1 and 100.")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess < number:
+        print("Too low! Try again.")
+
+    elif guess > number:
+        print("Too high! Try again.")
+
+    else:
+        print("🎉 Congratulations!")
+        print("You guessed the number in", attempts, "attempts.")
+        break
